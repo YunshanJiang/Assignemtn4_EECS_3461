0608e229803521b3dc46b71351f223f134875b34
YunshanJiang <yunshantree@163.com>
2019-11-20 20:46:24 -0500

    add android studio file
    add new file

--- app/src/main/java/com/example/a4chatapp/MainActivity.java
+++ app/src/main/java/com/example/a4chatapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.a4chatapp;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
