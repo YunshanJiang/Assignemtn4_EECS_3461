b8bd0b5bbf384bce6050e62c92674afabf222cb8
beinLogos <kci1016@my.yorku.ca>
2019-11-23 22:34:41 -0500

    Added Communcation Model and Junit Test file

---
+++ app/src/main/java/com/example/a4chatapp/CommunicationModel.java
@@ -0,0 +1,40 @@
+package com.example.a4chatapp;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CommunicationModel {
+
+    private static HashMap<String, String> responseMap;
+    static {
+        responseMap = new HashMap<>();
+        responseMap.put("(hi)+?", "Hello!");
+        responseMap.put("(how)+(you)?", "I'm good; how about you? how's your game play? did you find some hidden gold? I need some money to buy a rifle, but I don’t know where it is");
+        responseMap.put("(link|info|way)+(get|money|gold)*", "Yes, I need that information.");
+        responseMap.put("http", "this is great. But it is hard to see the map for the gold bar.");
+        responseMap.put("(give|wait)+(second)*", "yup!");
+        responseMap.put("map", "Thanks again!!");
+        responseMap.put("download", "Okay, I will download..");
+        responseMap.put("jpg", "This will be helpful");
+        responseMap.put("enjoy", "I'm gonna play right now! See you Isabelle!");
+        responseMap.put("(game)", "I found all the gold bars! thanks Isabelle!");
+        responseMap.put("(see).(you)", "See you! bye!");
+    }
+    private static final String defaultMessageFromBot = "I am sorry.. I don't know what you mean...";
+
+    public String getResponse(String userInput){
+        for(String keyReg: responseMap.keySet()){
+            Pattern p = Pattern.compile(keyReg);
+            Matcher m = p.matcher(userInput);
+
+            if(m.find())
+                return responseMap.get(keyReg);
+        }
+        return defaultMessageFromBot;
+    }
+
+    public static HashMap<String, String> getResponseMap() {
+        return responseMap;
+    }
+}
---
+++ app/src/test/java/ModelTest.java
@@ -0,0 +1,22 @@
+import com.example.a4chatapp.CommunicationModel;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class ModelTest {
+    @Test
+    public void communicationModelTest(){
+        CommunicationModel communicationModel = new CommunicationModel();
+        System.out.println("hi!\n" + communicationModel.getResponse("hi?"));
+        System.out.println("how are you?\n" + communicationModel.getResponse("how are you?"));
+        System.out.println("I have some information for gold bar.\n" + communicationModel.getResponse("I have some info for gold bar"));
+        System.out.println("do you want the information?\n" + communicationModel.getResponse("do you want the info?"));
+        System.out.println(communicationModel.getResponse("https://something youtube"));
+        System.out.println(communicationModel.getResponse("give me a second"));
+        System.out.println(communicationModel.getResponse("this is the map"));
+        System.out.println(communicationModel.getResponse("do you want to download the file?"));
+        System.out.println(communicationModel.getResponse("how's the game?"));
+        System.out.println(communicationModel.getResponse("ok~ see you"));
+    }
+}
