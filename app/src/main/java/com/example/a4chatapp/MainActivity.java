2dee4fa0025a1ae0df0115e672707cdf591a48eb
YunshanJiang <yunshantree@163.com>
2019-11-25 19:06:27 -0500

    createnewactivity

--- app/src/main/java/com/example/a4chatapp/MainActivity.java
+++ app/src/main/java/com/example/a4chatapp/MainActivity.java
@@ -11,9 +11,11 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button loginButton;
+    private Button CreateAccountButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
