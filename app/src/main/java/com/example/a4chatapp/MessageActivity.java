938e94bcf6af06a2d3af58233e35c1027dd820aa
YunshanJiang <yunshantree@163.com>
2019-11-25 20:45:48 -0500

    fragment&pager

--- app/src/main/java/com/example/a4chatapp/MessageActivity.java
+++ app/src/main/java/com/example/a4chatapp/MessageActivity.java
@@ -1,14 +1,34 @@
 package com.example.a4chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MessageActivity extends AppCompatActivity {
+import com.example.a4chatapp.fragments.pageFragment1;
+import com.example.a4chatapp.fragments.pageFragment2;
+import com.example.a4chatapp.fragments.pageFragment3;
+import com.example.a4chatapp.fragments.pageFragment4;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageActivity extends AppCompatActivity {
+private ViewPager pager;
+private PagerAdapter pagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        List<Fragment> list = new ArrayList<>();
+        list.add(new pageFragment1());
+        list.add(new pageFragment2());
+        list.add(new pageFragment3());
+        list.add(new pageFragment4());
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(),list);
+        pager.setAdapter(pagerAdapter);
     }
 }
---
+++ app/src/main/java/com/example/a4chatapp/fragments/pageFragment4.java
@@ -0,0 +1,21 @@
+package com.example.a4chatapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.a4chatapp.R;
+
+public class pageFragment4 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.profilepage, container, false);
+        return rootView;
+    }
+}
