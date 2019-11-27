ec2dbf2b4ba75b2758da0313a5ab3b7b477dadbc
beinLogos <kci1016@my.yorku.ca>
2019-11-26 19:07:44 -0500

    Merge remote-tracking branch 'remotes/origin/Davidtest'
    # Conflicts:
    #	app/src/main/java/com/example/a4chatapp/MainActivity.java

--- app/src/main/java/com/example/a4chatapp/MainActivity.java
+++ app/src/main/java/com/example/a4chatapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a4chatapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,6 +14,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import Persistent.UserAccountModel;
+import Service.UserAccountServiceImpl;
 
 public class MainActivity extends AppCompatActivity {
     private Button loginButton;
@@ -33,11 +39,20 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                CreateMessageActivity();
+                EditText userId = (EditText) findViewById(R.id.userId);
+                EditText password = (EditText) findViewById(R.id.password);
+                TextView validationMessage = (TextView) findViewById(R.id.validationMessage);
+                if(accountCheck(userId.getText().toString(), password.getText().toString())){
+                    validationMessage.setTextColor(Color.parseColor("#00A829"));
+                    validationMessage.setText("Login Suceed!");
+                    CreateMessageActivity();
+                } else {
+                    validationMessage.setText("* Id or Password Not Correct.");
+                }
             }
         });
     }
-private void CreateAccountActivity(){
+    private void CreateAccountActivity(){
         Intent intent = new Intent(this, NewAccountActivity.class);
         startActivity(intent);
     }
@@ -45,5 +60,14 @@ private void CreateAccountActivity(){
         Intent intent = new Intent(this, MessageActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private UserAccountServiceImpl userAccountServiceImpl = new UserAccountServiceImpl();
+    private boolean accountCheck(String userId, String password){
+        UserAccountModel.UserAccount ua = userAccountServiceImpl.retrieveUserAccountByUserId(userId);
+        if(ua != null && password.equals(ua.getPassword())){
+            return true;
+        }
+        return false;
     }
 }
