1d329b15c16bfbba29670ed53eabdea21bbbbc38
beinLogos <kci1016@my.yorku.ca>
2019-11-26 20:27:43 -0500

    Merge remote-tracking branch 'remotes/origin/Davidtest'
    # Conflicts:
    #	app/src/main/java/com/example/a4chatapp/MainActivity.java

--- app/src/main/java/com/example/a4chatapp/MainActivity.java
+++ app/src/main/java/com/example/a4chatapp/MainActivity.java
@@ -3,19 +3,12 @@ package com.example.a4chatapp;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import Persistent.UserAccountModel;
 import Service.UserAccountServiceImpl;
@@ -23,6 +16,7 @@ import Service.UserAccountServiceImpl;
 public class MainActivity extends AppCompatActivity {
     private Button loginButton;
     private Button CreateAccountButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/a4chatapp/fragments/pageFragment4.java
+++ app/src/main/java/com/example/a4chatapp/fragments/pageFragment4.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +12,37 @@ import androidx.fragment.app.Fragment;
 
 import com.example.a4chatapp.R;
 
+import Persistent.UserAccountModel;
+import Service.UserAccountServiceImpl;
+
 public class pageFragment4 extends Fragment {
+    private UserAccountServiceImpl uas = new UserAccountServiceImpl();
+    private UserAccountModel.UserAccount userAccount;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.profilepage, container, false);
+        setUserInfo(rootView);
         return rootView;
+    }
+
+    private void setUserInfo(ViewGroup root){
+        loadUserResultById("isabelle");
+        TextView userName =  root.findViewById(R.id.userName);
+        TextView userId = root.findViewById(R.id.userId);
+        TextView email = root.findViewById(R.id.email);
+        TextView address = root.findViewById(R.id.address);
+        TextView status = root.findViewById(R.id.status);
+
+        userName.setText(userAccount.getUserName());
+        userId.setText(userAccount.getUserId());
+        email.setText(userAccount.getEmail());
+        address.setText(userAccount.getAddress());
+        status.setText(userAccount.getStatus());
+
+    }
+
+    private void loadUserResultById(String userId){
+        userAccount = uas.retrieveUserAccountByUserId(userId);
     }
 }
