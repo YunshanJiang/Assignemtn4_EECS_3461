41db677368109c32d76ba2af591d4c3deb57a8c9
YunshanJiang <yunshantree@163.com>
2019-11-30 04:10:07 -0500

    discovery page and chat page
    chat page has bug temeporay

--- app/src/main/java/com/example/a4chatapp/ChatActivity.java
+++ app/src/main/java/com/example/a4chatapp/ChatActivity.java
@@ -1,13 +1,22 @@
 package com.example.a4chatapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChatActivity extends AppCompatActivity {
@@ -15,6 +24,11 @@ public class ChatActivity extends AppCompatActivity {
 
     ImageButton btn_send;
     EditText text_send;
+
+    MessageAdapter messageAdapter;
+    List<Chat> mchat;
+
+    RecyclerView recyclerView;
 
     Intent intent;
     @Override
@@ -33,12 +47,47 @@ public class ChatActivity extends AppCompatActivity {
                 finish();
             }
         });
+        mchat = new ArrayList<>();
+        mchat.add(new Chat("a", "a", "hellod"));
 
+        recyclerView = findViewById(R.id.recycler_view_chat);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        messageAdapter = new MessageAdapter(ChatActivity.this, mchat);
+        recyclerView.setAdapter(messageAdapter);
         username = findViewById(R.id.userIdInChat);
         btn_send = findViewById(R.id.btn_send);
         text_send = findViewById(R.id.text_send);
         intent = getIntent();
         String userid = intent.getStringExtra("username");
         username.setText(userid);
+
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String msg = text_send.getText().toString();
+                if (!msg.equals("")){
+                mchat.add(new Chat("a", "a", msg));
+                    messageAdapter = new MessageAdapter(ChatActivity.this, mchat);
+                    recyclerView.setAdapter(messageAdapter);
+                }
+                text_send.setText("");
+            }
+        });
     }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+
+    }
+
+    private void readMesagges(){
+   // messageAdapter = new MessageAdapter(ChatActivity.this, mchat);
+    }
+
 }
---
+++ app/src/main/java/com/example/a4chatapp/DiscoveryViewAdapter.java
@@ -0,0 +1,75 @@
+package com.example.a4chatapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class DiscoveryViewAdapter extends RecyclerView.Adapter<DiscoveryViewAdapter.MyViewHolder> {
+
+    Context mCotext;
+    List<Discovery> mNews;
+
+
+    public  DiscoveryViewAdapter(Context mContext, List<Discovery> mNews){
+        this.mCotext = mContext;
+        this.mNews = mNews;
+    }
+
+    @NonNull
+    @Override
+    public DiscoveryViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        v = LayoutInflater.from(mCotext).inflate(R.layout.discoveryitem, parent,false);
+        final DiscoveryViewAdapter.MyViewHolder viewHolder = new DiscoveryViewAdapter.MyViewHolder(v);
+        viewHolder.discovery_block.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiscoveryViewAdapter.MyViewHolder holder, int position) {
+        final Discovery discovery = mNews.get(position);
+        holder.discovery_text.setText(mNews.get(position).getDescription());
+        holder.discovery_image.setImageResource(mNews.get(position).getPhoto());
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(discovery.getUrl()));
+                mCotext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNews.size();
+    }
+
+    public  class MyViewHolder extends RecyclerView.ViewHolder{
+        public LinearLayout discovery_block;
+        public TextView discovery_text;
+        public ImageView discovery_image;
+
+        public  MyViewHolder(View itemView){
+            super(itemView);
+            discovery_block = (LinearLayout) itemView.findViewById(R.id.discovery_block);
+            discovery_text = (TextView) itemView.findViewById(R.id.discovery_text);
+            discovery_image = (ImageView) itemView.findViewById(R.id.discovery_image);
+        }
+    }
+}
