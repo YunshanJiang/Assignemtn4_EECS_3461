f9eb48b0c400b35c269788aa606d39984ff35d10
YunshanJiang <yunshantree@163.com>
2019-12-03 16:55:58 -0500

    add addfriend list

--- app/src/main/java/com/example/a4chatapp/AddFriendActivity.java
+++ app/src/main/java/com/example/a4chatapp/AddFriendActivity.java
@@ -3,6 +3,7 @@ package com.example.a4chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddFriendActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class AddFriendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+        androidx.appcompat.widget.Toolbar toorbar = findViewById(R.id.chatToorbar);
+
+        setSupportActionBar(toorbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toorbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
